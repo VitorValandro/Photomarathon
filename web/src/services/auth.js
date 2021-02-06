22ae5b2a82aa7f9e63fd0af3fe8569b91bcc6662
Vitor Valandro <vitormateusd@gmail.com>
2021-02-06 13:46:39 -0300

    Fix token expiration bug

--- web/src/services/auth.js
+++ web/src/services/auth.js
@@ -1,16 +1,22 @@
 export const TOKEN_KEY = "@team-Token";
-export const TEAM_KEY = "@team-Identifier"
+export const TEAM_KEY = "@team-Identifier";
+export const TOKEN_CHECKIN = "@team-Token-CheckInDate";
 
-export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+export const isAuthenticated = () => {
+  var OneDayFromNow = new Date().getTime() + (1 * 24 * 60 * 60 * 1000)
+  return localStorage.getItem(TOKEN_KEY) !== null && localStorage.getItem(TOKEN_CHECKIN) < OneDayFromNow; 
+}
 export const getTeamThatIsAuthenticated = () => localStorage.getItem(TEAM_KEY);
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 
 export const login = (token, teamId) => {
   localStorage.setItem(TOKEN_KEY, token);
   localStorage.setItem(TEAM_KEY, teamId);
+  localStorage.setItem(TOKEN_CHECKIN, Date.now());
 };
 
 export const logout = () => {
   localStorage.removeItem(TOKEN_KEY);
   localStorage.removeItem(TEAM_KEY);
+  localStorage.removeItem(TOKEN_CHECKIN);
 };
